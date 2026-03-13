a632f2ba063fa8254977476b01d6ff9cbfe6dc7c
hassamk122 <hassamkiani66@gmail.com>
2026-03-13 15:37:06 +0500

    utils to respond with success or error

---
+++ Backend/internals/utils/error_response.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type ErrorResponse struct {
+	Message string `json:"message"`
+}
+
+func RespondWithError(res http.ResponseWriter, statusCode int, message string) {
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(statusCode)
+	json.NewEncoder(res).Encode(ErrorResponse{
+		Message: message,
+	})
+}
+
+func RespondWithNotFound(res http.ResponseWriter) {
+	RespondWithError(res, http.StatusNotFound, "Resource not found")
+}
